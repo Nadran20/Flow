6d5efc12ff450fb1af1998cd7cf4da84f6df76ff
audran.liard2 <audran.liard2@gmail.com>
2024-08-26 13:33:42 +0000

    dev - remove useless class progress bar

--- src/components/progress/progress.tsx
+++ src/components/progress/progress.tsx
@@ -15,8 +15,8 @@ const Progress: React.FC<ProgressProps> = ({ value, max = 100 }) => {
 
     return (
         <div className="flex relative overflow-hidden rounded-xl">
-            <div className="absolute flex justify-center items-center inset-x-0 inset-y-0 bg-slate-200">{pourcente}%</div>
-            <div className="flex w-full justify-center items-center bg-zinc-700 text-white" style={style}>{pourcente}%</div>
+            <div className="absolute text-center inset-x-0 inset-y-0 bg-slate-200">{pourcente}%</div>
+            <div className="w-full text-center bg-zinc-700 text-white" style={style}>{pourcente}%</div>
         </div>
     );
 };
